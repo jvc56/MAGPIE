e0323bef40697c7b5ccef0d7f4d45f1e5c358c95
jvc56 <joshuacastellano7@gmail.com>
2024-07-03 00:36:52 -0400

    Refactor recorder

--- src/ent/autoplay_results.c
+++ src/ent/autoplay_results.c
@@ -10,24 +10,27 @@
 
 #include "../util/util.h"
 
-typedef void (*recorder_reset_func_t)(void *);
-typedef void *(*recorder_create_func_t)(void);
-typedef void (*recorder_destroy_func_t)(void *);
-typedef void (*recorder_add_move_func_t)(void *, const Move *);
-typedef void (*recorder_add_game_func_t)(void *, const Game *);
-typedef void (*recorder_combine_func_t)(void **, int, void *);
-typedef char *(*recorder_str_func_t)(void *, bool);
+typedef struct Recorder Recorder;
 
-typedef struct Recorder {
+typedef void (*recorder_reset_func_t)(Recorder *);
+typedef void (*recorder_create_data_func_t)(Recorder *, const Recorder *);
+typedef void (*recorder_destroy_data_func_t)(Recorder *);
+typedef void (*recorder_add_move_func_t)(Recorder *, const Move *);
+typedef void (*recorder_add_game_func_t)(Recorder *, const Game *);
+typedef void (*recorder_combine_func_t)(Recorder **, int, Recorder *);
+typedef char *(*recorder_str_func_t)(Recorder *, bool);
+
+struct Recorder {
   void *data;
+  void *shared_data;
+  bool owns_shared_data;
   recorder_reset_func_t reset_func;
-  recorder_create_func_t create_func;
-  recorder_destroy_func_t destroy_func;
+  recorder_destroy_data_func_t destroy_data_func;
   recorder_add_move_func_t add_move_func;
   recorder_add_game_func_t add_game_func;
   recorder_combine_func_t combine_func;
   recorder_str_func_t str_func;
-} Recorder;
+};
 
 struct AutoplayResults {
   uint64_t options;
@@ -36,12 +39,12 @@ struct AutoplayResults {
 
 // Generic recorders
 
-void add_move_noop(void __attribute__((unused)) * data,
+void add_move_noop(Recorder __attribute__((unused)) * recorder,
                    const Move __attribute__((unused)) * move) {
   return;
 }
 
-void add_game_noop(void __attribute__((unused)) * data,
+void add_game_noop(Recorder __attribute__((unused)) * recorder,
                    const Game __attribute__((unused)) * game) {
   return;
 }
@@ -58,8 +61,8 @@ typedef struct GameData {
   Stat *p1_score;
 } GameData;
 
-void game_data_reset(void *data) {
-  GameData *gd = (GameData *)data;
+void game_data_reset(Recorder *recorder) {
+  GameData *gd = (GameData *)recorder->data;
   gd->total_games = 0;
   gd->p0_wins = 0;
   gd->p0_losses = 0;
@@ -69,28 +72,31 @@ void game_data_reset(void *data) {
   stat_reset(gd->p1_score);
 }
 
-void *game_data_create(void) {
+void game_data_create(Recorder *recorder, const Recorder
+                                              __attribute__((unused)) *
+                                              primary_recorder) {
   GameData *game_data = malloc_or_die(sizeof(GameData));
   game_data->p0_score = stat_create(true);
   game_data->p1_score = stat_create(true);
-  game_data_reset(game_data);
-  return (void *)game_data;
+  recorder->data = game_data;
+  recorder->shared_data = NULL;
+  game_data_reset(recorder);
 }
 
-void game_data_destroy(void *data) {
-  if (!data) {
+void game_data_destroy(Recorder *recorder) {
+  if (!recorder) {
     return;
   }
-  GameData *gd = (GameData *)data;
+  GameData *gd = (GameData *)recorder->data;
   stat_destroy(gd->p0_score);
   stat_destroy(gd->p1_score);
   free(gd);
 }
 
-void game_data_add_game(void *data, const Game *game) {
+void game_data_add_game(Recorder *recorder, const Game *game) {
   int p0_game_score = player_get_score(game_get_player(game, 0));
   int p1_game_score = player_get_score(game_get_player(game, 1));
-  GameData *gd = (GameData *)data;
+  GameData *gd = (GameData *)recorder->data;
   gd->total_games++;
   if (p0_game_score > p1_game_score) {
     gd->p0_wins++;
@@ -106,26 +112,28 @@ void game_data_add_game(void *data, const Game *game) {
   stat_push(gd->p1_score, (double)p1_game_score, 1);
 }
 
-void game_data_combine(void **data_list, int data_list_size,
-                       void *data_target) {
-  Stat **p0_score_stats = malloc_or_die((sizeof(Stat *)) * (data_list_size));
-  Stat **p1_score_stats = malloc_or_die((sizeof(Stat *)) * (data_list_size));
+void game_data_combine(Recorder **recorder_list, int recorder_list_size,
+                       Recorder *primary_recorder) {
+  Stat **p0_score_stats =
+      malloc_or_die((sizeof(Stat *)) * (recorder_list_size));
+  Stat **p1_score_stats =
+      malloc_or_die((sizeof(Stat *)) * (recorder_list_size));
 
-  GameData *gd_target = (GameData *)data_target;
+  GameData *gd_primary = (GameData *)primary_recorder->data;
 
-  for (int i = 0; i < data_list_size; i++) {
-    GameData *gd_i = (GameData *)data_list[i];
-    gd_target->total_games += gd_i->total_games;
-    gd_target->p0_wins += gd_i->p0_wins;
-    gd_target->p0_losses += gd_i->p0_losses;
-    gd_target->p0_ties += gd_i->p0_ties;
-    gd_target->p0_firsts += gd_i->p0_firsts;
+  for (int i = 0; i < recorder_list_size; i++) {
+    GameData *gd_i = (GameData *)recorder_list[i]->data;
+    gd_primary->total_games += gd_i->total_games;
+    gd_primary->p0_wins += gd_i->p0_wins;
+    gd_primary->p0_losses += gd_i->p0_losses;
+    gd_primary->p0_ties += gd_i->p0_ties;
+    gd_primary->p0_firsts += gd_i->p0_firsts;
     p0_score_stats[i] = gd_i->p0_score;
     p1_score_stats[i] = gd_i->p1_score;
   }
 
-  stats_combine(p0_score_stats, data_list_size, gd_target->p0_score);
-  stats_combine(p1_score_stats, data_list_size, gd_target->p1_score);
+  stats_combine(p0_score_stats, recorder_list_size, gd_primary->p0_score);
+  stats_combine(p1_score_stats, recorder_list_size, gd_primary->p1_score);
   free(p0_score_stats);
   free(p1_score_stats);
 }
@@ -172,8 +180,8 @@ char *game_data_human_readable_str(const GameData *gd) {
   return ret_str;
 }
 
-char *game_data_str(void *data, bool human_readable) {
-  GameData *gd = (GameData *)data;
+char *game_data_str(Recorder *recorder, bool human_readable) {
+  GameData *gd = (GameData *)recorder->data;
   if (human_readable) {
     return game_data_human_readable_str(gd);
   } else {
@@ -183,23 +191,23 @@ char *game_data_str(void *data, bool human_readable) {
 
 // Generic recorder and autoplay results functions
 
-Recorder *recorder_create(recorder_reset_func_t reset_func,
-                          recorder_create_func_t create_func,
-                          recorder_destroy_func_t destroy_func,
+Recorder *recorder_create(const Recorder *primary_recorder,
+                          recorder_reset_func_t reset_func,
+                          recorder_create_data_func_t create_data_func,
+                          recorder_destroy_data_func_t destroy_data_func,
                           recorder_add_move_func_t add_move_func,
                           recorder_add_game_func_t add_game_func,
                           recorder_combine_func_t combine_func,
                           recorder_str_func_t str_func) {
   Recorder *recorder = malloc_or_die(sizeof(Recorder));
   recorder->reset_func = reset_func;
-  recorder->create_func = create_func;
-  recorder->destroy_func = destroy_func;
+  recorder->destroy_data_func = destroy_data_func;
   recorder->add_move_func = add_move_func;
   recorder->add_game_func = add_game_func;
   recorder->combine_func = combine_func;
   recorder->str_func = str_func;
-
-  recorder->data = create_func();
+  recorder->owns_shared_data = !primary_recorder;
+  create_data_func(recorder, primary_recorder);
 
   return recorder;
 }
@@ -208,34 +216,27 @@ void recorder_destroy(Recorder *recorder) {
   if (!recorder) {
     return;
   }
-  recorder->destroy_func(recorder->data);
+  recorder->destroy_data_func(recorder);
   free(recorder);
 }
 
-void recorder_reset(Recorder *recorder) {
-  recorder->reset_func(recorder->data);
-}
+void recorder_reset(Recorder *recorder) { recorder->reset_func(recorder); }
 
 void recorder_add_move(Recorder *recorder, const Move *move) {
-  recorder->add_move_func(recorder->data, move);
+  recorder->add_move_func(recorder, move);
 }
 
 void recorder_add_game(Recorder *recorder, const Game *game) {
-  recorder->add_game_func(recorder->data, game);
+  recorder->add_game_func(recorder, game);
 }
 
 void recorder_combine(Recorder **recorder_list, int list_size,
-                      Recorder *target) {
-  void **data_list = malloc_or_die(sizeof(void *) * list_size);
-  for (int i = 0; i < list_size; i++) {
-    data_list[i] = recorder_list[i]->data;
-  }
-  target->combine_func(data_list, list_size, target->data);
-  free(data_list);
+                      Recorder *primary_recorder) {
+  primary_recorder->combine_func(recorder_list, list_size, primary_recorder);
 }
 
 char *recorder_str(Recorder *recorder, bool human_readable) {
-  return recorder->str_func(recorder->data, human_readable);
+  return recorder->str_func(recorder, human_readable);
 }
 
 AutoplayResults *autoplay_results_create() {
@@ -252,16 +253,22 @@ uint64_t autoplay_results_build_option(autoplay_recorder_t recorder_type) {
 
 void autoplay_results_set_recorder(
     AutoplayResults *autoplay_results, uint64_t options,
+    const AutoplayResults *primary_autoplay_results,
     autoplay_recorder_t recorder_type, recorder_reset_func_t reset_func,
-    recorder_create_func_t create_func, recorder_destroy_func_t destroy_func,
+    recorder_create_data_func_t create_data_func,
+    recorder_destroy_data_func_t destroy_data_func,
     recorder_add_move_func_t add_move_func,
     recorder_add_game_func_t add_game_func,
     recorder_combine_func_t combine_func, recorder_str_func_t str_func) {
   if (options & autoplay_results_build_option(recorder_type)) {
     if (!autoplay_results->recorders[recorder_type]) {
-      autoplay_results->recorders[recorder_type] =
-          recorder_create(reset_func, create_func, destroy_func, add_move_func,
-                          add_game_func, combine_func, str_func);
+      const Recorder *primary_recorder = NULL;
+      if (primary_autoplay_results) {
+        primary_recorder = primary_autoplay_results->recorders[recorder_type];
+      }
+      autoplay_results->recorders[recorder_type] = recorder_create(
+          primary_recorder, reset_func, create_data_func, destroy_data_func,
+          add_move_func, add_game_func, combine_func, str_func);
     }
   } else {
     recorder_destroy(autoplay_results->recorders[recorder_type]);
@@ -270,11 +277,12 @@ void autoplay_results_set_recorder(
 }
 
 void autoplay_results_set_options_int(AutoplayResults *autoplay_results,
-                                      uint64_t options) {
+                                      uint64_t options,
+                                      const AutoplayResults *primary) {
   autoplay_results_set_recorder(
-      autoplay_results, options, AUTOPLAY_RECORDER_TYPE_GAME, game_data_reset,
-      game_data_create, game_data_destroy, add_move_noop, game_data_add_game,
-      game_data_combine, game_data_str);
+      autoplay_results, options, primary, AUTOPLAY_RECORDER_TYPE_GAME,
+      game_data_reset, game_data_create, game_data_destroy, add_move_noop,
+      game_data_add_game, game_data_combine, game_data_str);
   autoplay_results->options = options;
 }
 
@@ -299,11 +307,12 @@ autoplay_status_t autoplay_results_set_options_with_splitter(
   }
 
   if (status == AUTOPLAY_STATUS_SUCCESS) {
-    autoplay_results_set_options_int(autoplay_results, options);
+    autoplay_results_set_options_int(autoplay_results, options, NULL);
   }
 
   return status;
 }
+
 autoplay_status_t
 autoplay_results_set_options(AutoplayResults *autoplay_results,
                              const char *options_str) {
@@ -320,7 +329,7 @@ autoplay_results_set_options(AutoplayResults *autoplay_results,
 AutoplayResults *
 autoplay_results_create_empty_copy(const AutoplayResults *orig) {
   AutoplayResults *autoplay_results = autoplay_results_create();
-  autoplay_results_set_options_int(autoplay_results, orig->options);
+  autoplay_results_set_options_int(autoplay_results, orig->options, orig);
   return autoplay_results;
 }
 
@@ -358,7 +367,7 @@ void autoplay_results_add_game(AutoplayResults *autoplay_results,
 }
 
 void autoplay_results_combine(AutoplayResults **autoplay_results_list,
-                              int list_size, AutoplayResults *target) {
+                              int list_size, AutoplayResults *primary) {
   Recorder **recorder_list = malloc_or_die(sizeof(Recorder *) * list_size);
   for (int i = 0; i < NUMBER_OF_AUTOPLAY_RECORDERS; i++) {
     if (!autoplay_results_list[0]->recorders[i]) {
@@ -367,7 +376,7 @@ void autoplay_results_combine(AutoplayResults **autoplay_results_list,
     for (int j = 0; j < list_size; j++) {
       recorder_list[j] = autoplay_results_list[j]->recorders[i];
     }
-    recorder_combine(recorder_list, list_size, target->recorders[i]);
+    recorder_combine(recorder_list, list_size, primary->recorders[i]);
   }
   free(recorder_list);
 }
