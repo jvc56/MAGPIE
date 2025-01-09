9aa68c134cfc50def15b25749868ab3202d729cc
jvc56 <jvc56@cornell.edu>
2025-01-08 23:27:52 -0500

    Fix stability

--- src/ent/equity.h
+++ src/ent/equity.h
@@ -8,7 +8,7 @@
 
 typedef int32_t Equity;
 
-#define EQUITY_RESOLUTION (1 << 16)
+#define EQUITY_RESOLUTION 10000
 #define EQUITY_MIN_VALUE INT32_MIN
 #define EQUITY_MAX_VALUE INT32_MAX
 #define EQUITY_MIN_DOUBLE ((double)EQUITY_MIN_VALUE / EQUITY_RESOLUTION)
@@ -21,7 +21,13 @@ static inline Equity double_to_equity(double x) {
   if (x > EQUITY_MAX_DOUBLE || x < EQUITY_MIN_DOUBLE) {
     log_fatal("equity value out of range: %f", x);
   }
-  return (Equity)(x * EQUITY_RESOLUTION);
+  double rounded = round(x * EQUITY_RESOLUTION);
+  if (rounded > EQUITY_MAX_VALUE) {
+    return EQUITY_MAX_VALUE;
+  } else if (rounded < EQUITY_MIN_VALUE) {
+    return EQUITY_MIN_VALUE;
+  }
+  return (Equity)rounded;
 }
 
 static inline double equity_to_double(Equity eq) {
