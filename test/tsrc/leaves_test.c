7ac700f1cd4046d1d4b99f38ffe9ee94d72eb39b
John O'Laughlin <olaughlin@gmail.com>
2025-01-13 08:33:16 -0800

    board_layout_default_test passes

--- test/tsrc/leaves_test.c
+++ test/tsrc/leaves_test.c
@@ -38,9 +38,9 @@ void test_leaves(void) {
     StringSplitter *leave_and_value = split_string(line, ',', true);
     rack_set_to_string(ld, rack, string_splitter_get_item(leave_and_value, 0));
     const Equity klv_leave_value = klv_get_leave_value(klv, rack);
-    assert(within_epsilon(
-        klv_leave_value,
-        double_to_equity(string_to_float(string_splitter_get_item(leave_and_value, 1)))));
+    assert(within_epsilon(klv_leave_value,
+                          double_to_equity(string_to_float(
+                              string_splitter_get_item(leave_and_value, 1)))));
 
     string_splitter_destroy(leave_and_value);
   }
