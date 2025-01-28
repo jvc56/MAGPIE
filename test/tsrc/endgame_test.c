99bf97d4b27230366b42e41a6d84f650e00269bf
John O'Laughlin <olaughlin@gmail.com>
2025-01-27 21:18:09 -0800

    revert endgame test

--- test/tsrc/endgame_test.c
+++ test/tsrc/endgame_test.c
@@ -124,7 +124,7 @@ void test_eldar_v_stick(void) {
       "cgp "
       "4EXODE6/1DOFF1KERATIN1U/1OHO8YEN/1POOJA1B3MEWS/5SQUINTY2A/4RHINO1e3V/"
       "2B4C2R3E/GOAT1D1E2ZIN1d/1URACILS2E4/1PIG1S4T4/2L2R4T4/2L2A1GENII3/"
-      "2A2T1L7/5E1A7/5D1M7 AEEIRUW/V 410/409 0 -lex CSW21;");
+      "2A2T1L7/5E1A7/5D1M7 AEEIRUW/V 410/409 0 -lex CSW19;");
   int plies = 9;
   EndgameSolver *solver = endgame_solver_create(
       config_get_thread_control(config), config_get_game(config),
@@ -163,10 +163,10 @@ void test_endgame(void) {
   test_solve_standard();
   test_very_deep();
   test_small_arena_realloc();
-  // Uncomment out more of these tests once we add more optimizations,
+  //  Uncomment out more of these tests once we add more optimizations,
   //  and/or if we can run the endgame tests in release mode.
   //  test_pass_first();
-  //test_vs_joey();
-  //test_eldar_v_stick();
+  // test_vs_joey();
+  // test_eldar_v_stick();
   log_set_level(LOG_WARN);
 }
