63cd3cb5c3aa0201fdeb9b67be1fbd8d43d95e20
John O'Laughlin <olaughlin@gmail.com>
2025-02-11 22:04:26 -0800

    reset playthrough to avoid uninit data in test

--- test/tsrc/wmp_move_gen_test.c
+++ test/tsrc/wmp_move_gen_test.c
@@ -113,6 +113,7 @@ void test_playthrough_bingo_existence(void) {
   leave_map_set_current_index(&leave_map, 0);
 
   wmp_move_gen_init(&wmg, ld, rack, wmp);
+  wmp_move_gen_reset_playthrough(&wmg);
   assert(wmp_move_gen_is_active(&wmg));
   assert(!wmp_move_gen_has_playthrough(&wmg));
   // Add a letter, N. In this context we would be shadowing left.
