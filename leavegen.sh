03a90f3ed3a9fd787db7dbf990e1df802f4f8b76
jvc56 <jvc56@cornell.edu>
2025-02-13 21:44:59 -0500

    Update leavegen script

--- leavegen.sh
+++ leavegen.sh
@@ -1,9 +1,9 @@
-
-git pull && make clean && make magpie BUILD=release
-if [ "$3" != "" ]; then
+# git pull && make clean && make magpie BUILD=release
+# ./bin/magpie convert klv2csv CSW24_gen_ CSW24_gen_ -ld english -lex CSW24
+if [ "$1" == "c" ]; then
     ./download_data.sh
-    # Create empty klv
     ./bin/magpie createdata klv CSW24 english
     ./convert_lexica.sh
+else 
+    ./bin/magpie leavegen $1 $2 -lex CSW24 -leaves CSW24 -hr true -threads 32 -pfreq 100000 -wmp true
 fi
-./bin/magpie leavegen $1 $2 -lex CSW24 -leaves CSW24 -hr true -threads 2 -pfreq 100000 -wmp
