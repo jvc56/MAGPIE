e2513c6df3a076b17ccb8716be279c3309869f9e
jvc56 <jvc56@cornell.edu>
2025-02-15 17:47:37 -0500

    Update leavegen script to delete old data

--- leavegen.sh
+++ leavegen.sh
@@ -1,7 +1,9 @@
 # git pull && make clean && make magpie BUILD=release
 if [ "$2" == "" ]; then
     ./download_data.sh
+    rm -rf data/lexica/$1.klv2
     ./bin/magpie createdata klv $1 english
+    rm -rf data/leaves/$1.csv
     ./bin/magpie convert klv2csv $1 $1 -ld english -lex $1
     ./convert_lexica.sh
 else 
