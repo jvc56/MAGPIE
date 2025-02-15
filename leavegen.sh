6fc5b48d972b3bc9da72d88c3ad04f47b5caac7f
jvc56 <jvc56@cornell.edu>
2025-02-15 17:37:41 -0500

    Update leavegen script to customize lexicon

--- leavegen.sh
+++ leavegen.sh
@@ -1,9 +1,9 @@
 # git pull && make clean && make magpie BUILD=release
-# ./bin/magpie convert klv2csv CSW24_gen_ CSW24_gen_ -ld english -lex CSW24
-if [ "$1" == "c" ]; then
+if [ "$2" == "" ]; then
     ./download_data.sh
-    ./bin/magpie createdata klv CSW24 english
+    ./bin/magpie createdata klv $1 english
+    ./bin/magpie convert klv2csv $1 $1 -ld english -lex $1
     ./convert_lexica.sh
 else 
-    ./bin/magpie leavegen $1 $2 -lex CSW24 -leaves CSW24 -hr true -threads 32 -pfreq 100000 -wmp true
+    ./bin/magpie leavegen $2 $3 -lex $1 -leaves $1 -hr true -threads 32 -pfreq 100000 -wmp true
 fi
