6a1044cfbba07fbed74925874298b50e4cda96f9
jvc56 <jvc56@cornell.edu>
2025-02-17 16:55:00 -0500

    Make lexicon and leaves distinct in leavegen script

--- leavegen.sh
+++ leavegen.sh
@@ -7,5 +7,5 @@ if [ "$2" == "" ]; then
     ./bin/magpie convert klv2csv $1 $1 -ld english -lex $1
     ./convert_lexica.sh
 else 
-    ./bin/magpie leavegen $2 $3 -lex $1 -leaves $1 -hr true -threads 32 -pfreq 100000 -wmp true
+    ./bin/magpie leavegen $3 $4 -lex $2 -leaves $1 -hr true -threads 32 -pfreq 100000 -wmp true
 fi
