3d9b7c5ed2eff34b46423b6527c0b1f49cf9901b
jvc56 <jvc56@cornell.edu>
2025-09-05 22:38:40 -0400

    Improve bag print function

--- src/str/bag_string.c
+++ src/str/bag_string.c
@@ -11,27 +11,20 @@ enum { BLANK_SORT_VALUE = 255 };
 
 void string_builder_add_bag(StringBuilder *bag_string_builder, const Bag *bag,
                             const LetterDistribution *ld) {
-  // FIXME: use get unseen bag function
   int ld_size = ld_get_size(ld);
-  Rack *bag_as_rack = rack_create(ld_size);
-
-  Bag *copied_bag = bag_duplicate(bag);
-  int number_of_tiles = bag_get_letters(bag);
-  for (int i = 0; i < number_of_tiles; i++) {
-    rack_add_letter(bag_as_rack, bag_draw_random_letter(copied_bag, 0));
-  }
-  bag_destroy(copied_bag);
+  int bag_letter_counts[MAX_ALPHABET_SIZE];
+  memset(bag_letter_counts, 0, sizeof(bag_letter_counts));
+  bag_increment_unseen_count(bag, bag_letter_counts);
 
   for (int i = 1; i < ld_size; i++) {
-    for (int j = 0; j < rack_get_letter(bag_as_rack, i); j++) {
+    for (int j = 0; j < bag_letter_counts[i]; j++) {
       string_builder_add_user_visible_letter(bag_string_builder, ld, i);
     }
   }
 
   // Print the blanks at the end
-  for (int i = 0; i < rack_get_letter(bag_as_rack, BLANK_MACHINE_LETTER); i++) {
+  for (int i = 0; i < bag_letter_counts[BLANK_MACHINE_LETTER]; i++) {
     string_builder_add_user_visible_letter(bag_string_builder, ld,
                                            BLANK_MACHINE_LETTER);
   }
-  rack_destroy(bag_as_rack);
 }
